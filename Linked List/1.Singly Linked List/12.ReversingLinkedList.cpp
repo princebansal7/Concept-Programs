1f55a02f275f33ae89722684a5994486f16aee7a
Prince <75045465+princebansal7@users.noreply.github.com>
2022-01-18 23:45:12 +0530

    Add files via upload

---
+++ Linked List/1.Singly Linked List/12.ReversingLinkedList.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *head) {   // TC: O(n) , SC: O(1)
+
+    if (head == NULL) {
+        cout << "Empty List!!\n";
+    }
+
+    cout << "Current LL is: \n";
+
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n\n";
+}
+
+//_____WAY-1 (Using Temp Array)_____________________________________
+
+Node *ReverseList(Node *head) {
+    vector<int> temp;
+    Node *curr = head;
+
+    if (head == NULL)
+        return NULL;
+
+    while (curr != NULL) {
+        temp.push_back(curr->data);
+        curr = curr->next;
+    }
+
+    curr = head;
+    int i = temp.size() - 1;
+
+    while (curr != NULL) {
+        curr->data = temp[i];
+        i--;
+        curr = curr->next;
+    }
+    return head;
+}
+
+int main()
+{
+
+#ifdef PRINCE
+    freopen("debug.txt", "w", stderr);
+#endif
+
+    int t; cin >> t;  // for test cases
+    int cnt = 1;
+
+    while (t--) {
+
+
+        cout << "TEST CASE " << cnt << ":-------------------" << "\n\n";
+
+        Node *head = NULL;
+
+        // head = new Node(10);
+        // head->next = new Node(20);
+        // head->next->next = new Node(30);
+        // head->next->next->next = new Node(40);
+        // head->next->next->next->next = new Node(50);
+        // head->next->next->next->next->next = new Node(60);
+
+
+        cout << "Before Reversing:\n";
+        printList(head);
+
+        head = ReverseList(head);
+
+        cout << "After Reversing:\n";
+        printList(head);
+
+        cout << endl;
+        cnt++;   // ignore
+
+    }
+    return 0;
+}
