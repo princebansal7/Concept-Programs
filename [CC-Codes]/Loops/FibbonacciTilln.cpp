f0844c23019b5a8e8ef9a00891c823aeefab0f32
Prince Bansal <prince.bansal008@gmail.com>
2022-03-19 14:45:36 +0530

    updated

--- [CC-Codes]/Loops/FibbonacciTilln.cpp
+++ [CC-Codes]/Loops/FibbonacciTilln.cpp
@@ -1,6 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
 #define nl "\n"
 #define ll long long
 
@@ -11,14 +13,16 @@ Fibonacci seq:
 
 */
 
-void PrintFib(int n) {
+void PrintFib(int n)
+{
 
     int first = 0;
     int second = 1;
 
     cout << first << " " << second << " ";
 
-    for (int i = 1; i <= n - 2; i++) { // running loop till nth term - 2 times only as fist two terms already printed
+    for (int i = 1; i <= n - 2; i++)
+    { // running loop till nth term - 2 times only as fist two terms already printed
 
         int next = first + second;
         cout << next << " ";
@@ -29,15 +33,12 @@ void PrintFib(int n) {
     cout << nl;
 }
 
-
 int main()
 {
-#ifdef PRINCE
-    freopen("debug.txt", "w", stderr);
-#endif
     fast_io
 
-    int n; cin >> n;
+        int n;
+    cin >> n;
 
     cout << "Fibonacci till " << n << "th term is:" << nl;
     PrintFib(n);
