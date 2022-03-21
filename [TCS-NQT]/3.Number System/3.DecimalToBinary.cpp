3206d42a9a2c67ef55e45cff6b4dcf39fd2f0407
Prince Bansal <prince.bansal008@gmail.com>
2022-03-21 17:20:10 +0530

    files added

---
+++ [TCS-NQT]/3.Number System/3.DecimalToBinary.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+//--------WAY-1---------------------------
+
+void decimalToBinary1(int n)
+{
+    bool bits[32] = {0};
+
+    int index = 0;
+    while (n)
+    {
+        bits[index] = n % 2;
+        index++;
+        n /= 2;
+    }
+    for (int i = index - 1; i >= 0; i--)
+        cout << bits[i];
+    cout << nl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    decimalToBinary(n);
+    return 0;
+}
