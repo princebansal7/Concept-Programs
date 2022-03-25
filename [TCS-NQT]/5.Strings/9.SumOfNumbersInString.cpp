4e4459ebf35f6a00968d7806ba5194882408ca9c
Prince Bansal <prince.bansal008@gmail.com>
2022-03-25 15:19:13 +0530

    file added

---
+++ [TCS-NQT]/5.Strings/9.SumOfNumbersInString.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#define nl "\n"
+#define ll long long
+
+/*
+        Calculating the sum of numbers in a string
+        (multiple consecutive digits are considered one number)
+        Example 1:
+        Input: string = “123xyz”
+        Output: 123
+
+        Example 2:
+        Input: string = “1xyz23”
+        Output: 24
+*/
+
+void removeBrackets(string &s)
+{
+    int sum = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        }
+}
+
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    removeBrackets(s);
+
+    return 0;
+}
