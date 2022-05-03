8fd7afdc50da37f69c08407d478e4847225c1782
Prince Bansal <prince.bansal008@gmail.com>
2022-05-03 12:31:05 +0530

    file added

--- [TCS-NQT]/1.Array/17.FindingAllNonRepeatingElements.cpp
+++ [TCS-NQT]/1.Array/17.FindingAllNonRepeatingElements.cpp
@@ -29,7 +29,52 @@ void findNonRepeated1(int *arr, int n)
     cout << nl;
 }
 
-// to be completed
+// Sorting Method   TC:O(NlogN)+O(N), SC: O(1)----------------------------------
+
+void findNonRepeated2(int *arr, int n)
+{
+    sort(arr, arr + n); // O(NlogN)
+
+    // Edge case: Checking for 1st element
+    if (arr[0] != arr[1])
+    {
+        cout << arr[0] << " ";
+    }
+
+    // checking for all b/w elements
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    // Edge case: Checking for last element
+    if (arr[n - 1] != arr[n - 2])
+    {
+        cout << arr[n - 1] << " ";
+    }
+    cout << nl;
+}
+
+// Using Hashmap    TC:O(N), SC: O(N)------------------------------
+
+void findNonRepeated3(int arr[], int n)
+{
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
+
+    for (auto val : mp)
+    {
+        if (val.second == 1)
+            cout << val.first << " ";
+    }
+    cout << nl;
+}
 
 int main()
 {
@@ -38,13 +83,11 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    findNonRepeated1(arr, n);
+    // findNonRepeated1(arr, n);
 
     // findNonRepeated2(arr, n);
 
-    // findNonRepeated3(arr, n);
-
-    // findNonRepeated4(arr, n);
+    findNonRepeated3(arr, n);
 
     return 0;
 }
