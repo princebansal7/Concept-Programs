2f8790e256b08d07f814c41d44e725d309db73f0
Prince Bansal <prince.bansal008@gmail.com>
2022-05-07 21:06:51 +0530

    incomplete code

--- [TCS-NQT]/1.Array/21.SortingArrayElementsByFrequency.cpp
+++ [TCS-NQT]/1.Array/21.SortingArrayElementsByFrequency.cpp
@@ -33,7 +33,7 @@ void sortByFreq1(int arr[], int n)
         mp[arr[i]]++;
 
     for (auto val : mp) {
-        while (val.second != 0) { // we just printed the hashmap values
+        while (val.second != 0) { // we just printed the hashmap values in sorted manner
             cout << val.first << " ";
             val.second--;
         }
