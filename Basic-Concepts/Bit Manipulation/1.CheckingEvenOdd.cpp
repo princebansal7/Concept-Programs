93a75e976de4eb015b899660e22e26682f692ae0
Prince Bansal <prince.bansal008@gmail.com>
2022-06-18 12:59:03 +0530

    updated

---
+++ Basic-Concepts/Bit Manipulation/1.CheckingEvenOdd.cpp
@@ -0,0 +1,19 @@
+/*
+    @author: princebansal_
+*/
+
+#include <iostream>
+using namespace std;
+#define nl '\n'
+#define ll long long
+
+int main()
+{
+    int a;
+    cin >> a;
+    if (a & 1)
+        cout << a << " is ODD" << nl;
+    else
+        cout << a << " is EVEN" << nl;
+    return 0;
+}
