339a42a294bb3a289bc8055ba1ced2c0641fcde3
Prince Bansal <prince.bansal008@gmail.com>
2022-06-28 20:51:25 +0530

    queue using Array added

--- [DSA-OOP]/4.Queue/QueueUsingArray/QueueArrayMain.cpp
+++ [DSA-OOP]/4.Queue/QueueUsingArray/QueueArrayMain.cpp
@@ -6,19 +6,40 @@ int main()
     q.createQueue(10);
 
     q.enQueue(10);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(20);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(30);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(40);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(50);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(60);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(70);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(80);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(90);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
     q.enQueue(100);
-    // q.deQueue();
-    q.enQueue(69);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
+    q.deQueue();
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
+    q.enQueue(111);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
+    q.deQueue();
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
+    q.enQueue(222);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
+    q.deQueue();
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
+    q.enQueue(333);
+    cout << "rear: " << q.getRear() << " front: " << q.getFront() << nl;
 
-    cout << nl << "checking isFull: " << q.isQueueFull() << nl << nl;
+    cout << nl << "checking isFull: " << q.isQueueFull() << nl;
+    cout << nl << "checking isEmpty: " << q.isQueueEmpty() << nl << nl;
 
     while (!q.isQueueEmpty()) {
         cout << "size: " << q.getSize() << nl;
