6dea443ffac04f7605bf891effd75243cc42b167
Prince Bansal <prince.bansal008@gmail.com>
2022-06-30 19:24:54 +0530

    sorting class added

---
+++ [DSA-OOP]/7.Sorting/SortingMain.cpp
@@ -0,0 +1,26 @@
+
+#include "SortingClass.cpp"
+
+int arr[100];
+
+int main()
+{
+
+    int size;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+
+    // Sorting::BubbleSort(arr, size);
+    // Sorting::ModifiedBubbleSort(arr, size);
+    // Sorting::insertionSort(arr, size);
+    // Sorting::selectionSort(arr, size);
+    Sorting::mergeSort(arr, 0, size - 1);
+
+    cout << "After Sorting: " << nl;
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+
+    cout << nl;
+    return 0;
+}
